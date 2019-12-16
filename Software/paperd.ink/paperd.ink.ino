8a36ad8f1095b9f1ca835003c286ab4cea6f1db3
Rohit Gujarathi <gujju.rohit@gmail.com>
2019-12-17 00:30:24 +0530

    fixed weather not being displayed

--- Software/paperd.ink/paperd.ink.ino
+++ Software/paperd.ink/paperd.ink.ino
@@ -48,10 +48,10 @@ extern "C" {
 #define RED 5
 #define TZ_ENV "UTC-05:30" //See: https://www.gnu.org/software/libc/manual/html_node/TZ-Variable.html  
 #define TIME_TO_SLEEP  55        //Time ESP32 will go to sleep (in seconds)
-const char* ssid = "f50c1ety";
-const char* password =  "studnigu";
-const char* todoist_token = "Bearer 3326738d4cdd28a7923f4aff6559bdb61155bc8c";
-const char* openweathermap_link = "http://api.openweathermap.org/data/2.5/weather?q=Pune,india&appid=6f08bcf115cc4d5ee1c5b4967a1d6279";
+const char* ssid = "*************";
+const char* password =  "*************";
+const char* todoist_token = "Bearer *************";
+const char* openweathermap_link = "http://api.openweathermap.org/data/2.5/*************";
 
 // ###################################
 
